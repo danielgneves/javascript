d7d8889d4392afc52e546ab03450ffeb1fd0ca92
Daniel Neves <84115078+danielgneves@users.noreply.github.com>
2023-01-06 11:02:57 -0300

    ex 4

---
+++ aula14ex/ex016/script.js
@@ -0,0 +1,32 @@
+function contar() {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Verifique os dados e tente novamente!'
+        //window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        res.innerHTML = 'Contando: '
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando passo 1.')
+            p = 1
+        }
+        if (i < f) {
+            // Contagem crescente
+            for(let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} `
+            }
+        } else {
+            // Contagem decrescente
+            for(let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} `
+            }
+        }
+    }
+
+}
